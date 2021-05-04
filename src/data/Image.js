a8fd6876d03b66f7941792459a00fc3e2243aa80
unknown <tranthanhdanh0311@gmail.com>
2021-05-03 22:00:09 -0700

    oke

--- src/data/Image.js
+++ src/data/Image.js
@@ -7,7 +7,7 @@ export default function Image({result}) {
    return (
         <div className="image">
             <h2>Type: {type}</h2>
-            <h1>Name: {name}</h1>
+            <h2>Name: {name}</h2>
             {
                 photos.map(item =>{
                     return <SingleImage key={item.id} images={item} />
